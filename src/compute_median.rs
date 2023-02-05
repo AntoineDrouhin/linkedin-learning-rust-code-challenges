c82e6e8be1fa7bb3848498f5eba31f3d5b954116
Antoine Drouhin <antoine.drouhin@gmail.com>
2023-02-05 14:37:00 +0100

    compute median for unordered vectors

--- src/compute_median.rs
+++ src/compute_median.rs
@@ -1,11 +1,16 @@
-pub fn compute_median(vec: Vec<i32>) -> i32 {
+pub fn compute_median(vec: Vec<i32>) -> Result<i32, &'static str> {
     let length: usize = vec.len();
+
+    if length == 0 {return Err("Empty vector")}
+
+    let mut sorted_vec : Vec<i32> = vec.to_vec();
+    sorted_vec.sort();     
 
     let divisible_length = if length % 2 == 0 { length } else { length - 1 };
 
     let result_index = divisible_length / 2;
 
-    return vec[result_index];
+    return Ok(sorted_vec[result_index]);
 }
 
 #[cfg(test)]
@@ -13,7 +18,7 @@ mod tests {
     #[test]
     fn given_ordered_even_input_compute_median() {
         let input: Vec<i32> = vec![1, 4, 25, 46];
-        let result: i32 = super::compute_median(input);
+        let result: i32 = super::compute_median(input).unwrap();
 
         assert_eq!(result, 25);
     }
@@ -21,8 +26,41 @@ mod tests {
     #[test]
     fn given_ordered_odd_input_compute_median() {
         let input: Vec<i32> = vec![1, 4, 8, 25, 46];
-        let result: i32 = super::compute_median(input);
+        let result: i32 = super::compute_median(input).unwrap();
 
         assert_eq!(result, 8);
     }
+
+    #[test]
+    fn given_single_input_compute_median() {
+        let input: Vec<i32> = vec![8];
+        let result: i32 = super::compute_median(input).unwrap();
+
+        assert_eq!(result, 8);
+    }
+    
+    #[test]
+    fn handle_empty_vec() {
+        let input: Vec<i32> = vec![];
+        let result: &str = super::compute_median(input).unwrap_err();
+
+        assert_eq!(result, "Empty vector");
+    }
+#[test]
+    fn given_unordered_even_input_compute_median() {
+        let input: Vec<i32> = vec![46, 4, 1, 25];
+        let result: i32 = super::compute_median(input).unwrap();
+
+        assert_eq!(result, 25);
+    }
+
+#[test]
+    fn given_unordered_odd_input_compute_median() {
+        let input: Vec<i32> = vec![1, 4, 46, 25, 8];
+        let result: i32 = super::compute_median(input).unwrap();
+
+        assert_eq!(result, 8);
+    }
+
+    
 }
